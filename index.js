cf80284ef0d7b190b9ec69bc8a99f7b35e5bc412
ddsykes <ddsykes@hotmail.com>
2024-07-06 18:28:09 -0500

    Adding fles

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join((__dirname, "public", "index.html")));
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
